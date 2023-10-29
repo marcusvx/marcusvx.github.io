e6a30aa0da4b657dddf14454742c7ad7d8f376a7
Marcus V. Ximenes <marcus@marcusvinicius.info>
2023-10-29 12:55:13 -0300

    Full implementation of Bulma navbar

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,103 +1,72 @@
+<script setup>
+import { ref } from 'vue'
+
+const menuActive = ref(false)
+
+function toggleMenu() {
+  menuActive.value = !menuActive.value
+}
+</script>
+<style lang="scss">
+@import 'bulma/sass/utilities/_all.sass';
+@import 'bulma/sass/components/navbar';
+@import 'bulma/sass/components/menu';
+@import 'bulma/sass/form/shared';
+@import 'bulma/sass/form/select';
+@import 'bulma/sass/form/tools';
+@import 'bulma/sass/elements/icon';
+</style>
 <template>
-  <nav class="main-navbar" role="navigation" aria-label="main navigation">
-    <div class="container">
-      <div class="navbar-brand">
-        <a href="/" class="navbar-item">
-          <i class="fas fa-terminal"/>
-          marcusvinicius.info
-        </a>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a href="/" class="navbar-item">
+        <i class="fas fa-terminal" />
+        marcusvinicius.info
+      </a>
+      <a
+        role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="mainNavbar"
+        :class="menuActive ? 'is-active' : ''"
+        @click="toggleMenu"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="mainNavbar" class="navbar-menu" :class="menuActive ? 'is-active' : ''">
+      <div class="navbar-start">
+        <a class="navbar-item" href="#home"> {{ $t('menu.home') }} </a>
+        <a class="navbar-item" href="#about"> {{ $t('menu.about') }} </a>
+        <a class="navbar-item" href="#skills"> {{ $t('menu.skills') }} </a>
+        <a class="navbar-item" href="#experiences"> {{ $t('menu.experiences') }} </a>
+        <a class="navbar-item" href="#contact"> {{ $t('menu.contact') }} </a>
       </div>
+
       <div class="navbar-end">
-        <div class="language-options">
-          <!--        <button-->
-          <!--            class="language-dropdown-link is-arrowless"-->
-          <!--            @click="handleChangeLanguage">-->
-          <!--          <i class="fa-solid fa-language"/> {LANGUAGES[context.value]}-->
-          <!--        </button>-->
-          <!--        {languageOptions.map(language => (-->
-          <!--        <div :key="language.code" class="language-dropdown">-->
-          <!--          <button onClick="handleChangeLanguage">{language.name}</button>-->
-          <!--        </div>-->
-          <!--        ))}-->
-        </div>
         <div class="navbar-item">
-          <MainMenu/>
+          <div class="control has-icons-left">
+            <div class="select">
+              <select v-model="$i18n.locale">
+                <option
+                  v-for="locale in $i18n.availableLocales"
+                  :key="`locale-${locale}`"
+                  :value="locale"
+                >
+                  {{ $t(locale) }}
+                </option>
+              </select>
+            </div>
+            <span class="icon is-left">
+              <i class="fas fa-globe"></i>
+            </span>
+          </div>
         </div>
       </div>
     </div>
   </nav>
 </template>
-<style lang="scss">
-@import '../styles/index.scss';
-@import 'bulma/sass/components/navbar';
-@import 'bulma/sass/components/dropdown';
-@import 'bulma/sass/elements/container';
-@import 'bulma/sass/elements/button';
-@import 'bulma/sass/helpers/other';
-
-.main-navbar {
-  @extend .navbar;
-  @extend .is-fixed-top;
-
-  .navbar-item {
-    @extend .navbar-item;
-
-    i {
-      margin-right: 10px;
-    }
-  }
-
-  .navbar-end {
-    @extend .navbar-end;
-
-    .language-options {
-      @extend .navbar-item;
-      @extend .has-dropdown;
-      @extend .is-hoverable;
-
-      .is-arrowless {
-        @extend .navbar-link;
-      }
-
-      .language-dropdown-link {
-        font-size: 0.8rem;
-        border: none;
-        background-color: inherit;
-
-        i {
-          margin-right: 8px;
-        }
-
-        ::after {
-          border: none;
-        }
-      }
-
-      .language-dropdown {
-        @extend .navbar-dropdown;
-
-        button {
-          @extend .navbar-item;
-          @extend .is-clickable;
-          @extend .is-hoverable;
-          border: none;
-          background-color: inherit;
-          font-size: 0.8rem;
-        }
-      }
-
-      img:last-child {
-        margin-left: 5px;
-      }
-    }
-  }
-
-  .navbar-brand {
-    @extend .navbar-brand;
-  }
-}
-
-</style>
-<script setup>
-import MainMenu from '@/components/MainMenu.vue';
-</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import './styles/index.scss'
-import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/css/all.css'
+import en from '../public/locales/en-us.json'
+import pt from '../public/locales/pt-br.json'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createI18n } from 'vue-i18n'
 
-createApp(App).mount('#app')
+console.log(en)
+const i18n = createI18n({
+  locale: 'en',
+  fallbackLocale: 'pt',
+  messages: {
+    en,
+    pt
+  }
+})
+const app = createApp(App)
+app.use(i18n)
+app.mount('#app')
